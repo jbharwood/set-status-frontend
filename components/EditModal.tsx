14fbd8435dd0d00d58f5323a0299ff2915e8c2b4
Joseph Harwood <josephblakeharwood@gmail.com>
2024-12-28 18:26:05 -0500

    hid dialog description with radix visually hidden package

--- components/EditModal.tsx
+++ components/EditModal.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 
 type EditModalProps = {
   isEditModalOpen: boolean;
@@ -28,6 +29,11 @@ export default function EditModal({
         <DialogHeader>
           <DialogTitle>Edit Production Role Capture Status</DialogTitle>
         </DialogHeader>
+        <VisuallyHidden.Root>
+          <DialogDescription>
+            Edit Production Role Capture Status
+          </DialogDescription>
+        </VisuallyHidden.Root>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="name" className="text-right">
@@ -44,7 +50,6 @@ export default function EditModal({
           <Button type="submit">Save</Button>
         </DialogFooter>
       </DialogContent>
-      <DialogDescription>Edit Production Role Capture Status</DialogDescription>
     </Dialog>
   );
 }
