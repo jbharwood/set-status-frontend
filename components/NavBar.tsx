f4e4853b965cbf6f49df3feee628e32f34053217
Joseph Harwood <josephblakeharwood@gmail.com>
2024-12-28 13:14:11 -0500

    made navbar links go to the right side of the screen

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,9 +3,9 @@ import { UserButton } from "@clerk/nextjs";
 export default function NavBar() {
   return (
     <nav className="bg-gray-800 p-4 h-[5%]">
-      <div className="container flex justify-between items-center h-full">
+      <div className="flex items-center h-full">
         <div className="text-white text-lg font-semibold">Set Status</div>
-        <div className="space-x-4 flex">
+        <div className="space-x-4 flex ml-auto">
           <a href="#" className="text-gray-300 hover:text-white">
             Home
           </a>
